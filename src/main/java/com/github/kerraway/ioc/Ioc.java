87468d35a1d733dfa00d217362028732241a2115
kerraway <geekxiaoke@gmail.com>
2019-09-11 16:08:27 +0800

    *feature) init project.

---
+++ src/main/java/com/github/kerraway/ioc/Ioc.java
@@ -0,0 +1,8 @@
+package com.github.kerraway.ioc;
+
+/**
+ * @author kerraway
+ * @date 2019/09/11
+ */
+public class Ioc {
+}
---
+++ src/test/java/com/github/kerraway/ioc/IocTests.java
@@ -0,0 +1,18 @@
+package com.github.kerraway.ioc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author kerraway
+ * @date 2019/09/11
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+})
+public class IocTests {
+
+}
